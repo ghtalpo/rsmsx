33f761938ad4f404f1d618f917e30ed5b24cb126
gtalpo <gtalpo@gmail.com>
2025-01-14 15:19:50 +0900

    gen: ld, or, cp, add, adc, sub, ret, jp, jr

--- src/util/gen.py
+++ src/util/gen.py
@@ -53,6 +53,8 @@ def convert_to_lua(line):
                     if re.match(r'^[\d]', oprr[1]):
                         return "self.instr_hk__LD_%s_NN(%s);" % (oprr[0],oprr[1])
                         # return "z80.%s = %s" % (oprr[0],oprr[1])
+                    elif oprr[1].startswith("'") and oprr[1].endswith("'"):
+                        return "self.instr_hk__LD_%s_NN(%s as u32 as u8);" % (oprr[0],oprr[1])
                     elif oprr[1].startswith('('):
                         tgt = oprr[1][1:].rstrip(')').rstrip()
                         if is_reg16(tgt[:2]):
@@ -111,9 +113,9 @@ def convert_to_lua(line):
                     return "self.instr_hk__%s_A_%s();" % (op,opr)
                 elif re.match(r'^[\d]', opr):
                     return "self.instr_hk__%s_NN(%s);" % (op,opr)
-                    # return "z80:op_or(%s)" % (opr,)
                 elif opr[0] == '(' and is_reg16(opr[1:3]):
-                    return "z80:op_or(read_byte(z80, z80_gen.%s(z80)))" % (opr[1:3],)
+                    return "self.instr_hk__%s_A_i%s();" % (op,opr[1:3])
+                    # return "z80:op_or(read_byte(z80, z80_gen.%s(z80)))" % (opr[1:3],)
                 else:
                     return "WRONG %s %s" % (op,opr)
             elif op == 'AND':
@@ -126,7 +128,8 @@ def convert_to_lua(line):
                     return "WRONG %s %s" % (op,opr)
             elif op == 'CP':
                 if is_reg8(opr):
-                    return "z80:cp(z80.%s)" % opr
+                    return "self.instr_hk__%s_%s();" % (op, opr)
+                    # return "z80:cp(z80.%s)" % opr
                 elif opr.startswith('('):
                     tgt = opr[1:].rstrip(')').rstrip()
                     if is_reg16(tgt[:2]):
@@ -166,9 +169,12 @@ def convert_to_lua(line):
                         return "self.instr_hk__%s_A_%s();" % (op,oprr[1])
                         # return "z80:add(z80.%s)" % (oprr[1],)
                     elif oprr[1][0] == '(' and is_reg16(oprr[1][1:3]):
-                        return "z80:add(read_byte(z80, z80_gen.%s(z80)))" % (oprr[1][1:3],)
+                        # return "z80:add(read_byte(z80, z80_gen.%s(z80)))" % (oprr[1][1:3],)
+                        return "self.instr_hk__%s_A_i%s();" % (op, oprr[1][1:3])
+                    elif oprr[1].startswith("'") and oprr[1].endswith("'"):
+                        return "self.instr_hk__%s_A_NN(%s as u32 as u8);" % (op, oprr[1])
                     else:
-                        return "self.instr_hk__%s_A_NN(%s);" % (op,oprr[1])
+                        return "self.instr_hk__%s_A_NN(%s);" % (op, oprr[1])
                         # return "z80:add(%s)" % (oprr[1],)
                 # elif is_reg8(oprr[0]):
                     # return "z80.add(z80.%s)" % (oprr[1],)
@@ -182,7 +188,8 @@ def convert_to_lua(line):
                     if is_reg8(oprr[1]):
                         return "self.instr_hk__%s_A_%s();" % (op,oprr[1])
                     elif oprr[1][0] == '(' and is_reg16(oprr[1][1:3]):
-                        return "z80:adc(read_byte(z80, z80_gen.%s(z80)))" % (oprr[1][1:3],)
+                        return "self.instr_hk__%s_A_i%s();" % (op,oprr[1][1:3])
+                        # return "z80:adc(read_byte(z80, z80_gen.%s(z80)))" % (oprr[1][1:3],)
                     else:
                         return "z80:adc(%s)" % (oprr[1],)
                 # elif is_reg8(oprr[0]):
@@ -198,7 +205,7 @@ def convert_to_lua(line):
                 elif opr[0] == '(' and is_reg16(opr[1:3]):
                     return "self.instr_hk__%s_A_i%s();" % (op,opr[1:3])
                 else:
-                    return "self.instr_hk__%s_A_%s();" % (op,opr)
+                    return "self.instr_hk__%s_NN(%s);" % (op,opr)
                     # return "WRONG %s %s" % (op,opr)
             elif op == 'SBC':
                 oprr = opr.split(',')
@@ -225,16 +232,22 @@ def convert_to_lua(line):
             elif op == 'RET':
                 if opr == 'Z':
                     return "self.IncPC(1);\nif (self.data.F & FLAG_Z) != 0 {\n\tself.increase_cycles(11);return true;\n} else {\n\tself.increase_cycles(5);\n}"
+                elif opr == 'NZ':
+                    return "self.IncPC(1);\nif (self.data.F & FLAG_Z) == 0 {\n\tself.increase_cycles(11);return true;\n} else {\n\tself.increase_cycles(5);\n}"
                 else:
                     return "WRONGr? %s %s" % (op,opr)
             elif op == 'JP':
                 oprr = opr.split(',')
                 if len(oprr) == 1:
                     return "self.IncPC(3);self.increase_cycles(10);JP %s;\n" % oprr[0]
+                elif oprr[0] == 'M':
+                    return "self.IncPC(3);self.increase_cycles(10);\nif (self.data.F & FLAG_S) != 0 {\n\tJP %s;\n}\n" % oprr[1]
                 elif oprr[0] == 'Z':
                     return "self.IncPC(3);self.increase_cycles(10);\nif (self.data.F & FLAG_Z) != 0 {\n\tJP %s;\n}\n" % oprr[1]
                 elif oprr[0] == 'NZ':
                     return "self.IncPC(3);self.increase_cycles(10);\nif (self.data.F & FLAG_Z) == 0 {\n\tJP %s;\n}\n" % oprr[1]
+                elif oprr[0] == 'C':
+                    return "self.IncPC(3);self.increase_cycles(10);\nif (self.data.F & FLAG_C) != 0 {\n\tJP %s;\n}\n" % oprr[1]
                 elif oprr[0] == 'NC':
                     return "self.IncPC(3);self.increase_cycles(10);\nif (self.data.F & FLAG_C) == 0 {\n\tJP %s;\n}\n" % oprr[1]
                 else:
@@ -247,6 +260,8 @@ def convert_to_lua(line):
                     return "self.IncPC(2);\nif (self.data.F & FLAG_Z) != 0 {\n\tself.increase_cycles(12);JR %s;\n} else {\nself.increase_cycles(7);\n}\n" % oprr[1]
                 elif oprr[0] == 'NZ':
                     return "self.IncPC(2);\nif (self.data.F & FLAG_Z) == 0 {\n\tself.increase_cycles(12);JR %s;\n} else {\nself.increase_cycles(7);\n}\n" % oprr[1]
+                elif oprr[0] == 'C':
+                    return "self.IncPC(2);\nif (self.data.F & FLAG_C) != 0 {\n\tself.increase_cycles(12);JR %s;\n} else {\nself.increase_cycles(7);\n}\n" % oprr[1]
                 elif oprr[0] == 'NC':
                     return "self.IncPC(2);\nif (self.data.F & FLAG_C) == 0 {\n\tself.increase_cycles(12);JR %s;\n} else {\nself.increase_cycles(7);\n}\n" % oprr[1]
                 else:
